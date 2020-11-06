419d1026a3939d37b6f3670bc0e8374589407ae7
Soon Lai <soonlai814@gmail.com>
2020-11-07 07:05:02 +0800

    Fix build platform issues

--- ios/EuliTests/EuliTests.m
+++ ios/EuliTests/EuliTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ImbaTests : XCTestCase
+@interface EuliTests : XCTestCase
 
 @end
 
-@implementation ImbaTests
+@implementation EuliTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
